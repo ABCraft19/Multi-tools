9d8cf7e8b6d0d8684fdf5e2d76df42c2654b1bd9
ABCraft19 <lesenfantsbergaoui@gmail.com>
2024-01-01 13:03:25 +0100

    V0.1

---
+++ Multi-Tools/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Multi_Tools
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
